7edc59acf0a907fb7591ab36859d2fad1a214252
Lucas Fontenele <Lucas Fontenele>
2017-03-20 17:31:59 -0300

    Concluindo tela do solicitante, #Resolver problema do wizard

--- src/main/java/com/noworder/mb/CarService.java
+++ src/main/java/com/noworder/mb/CarService.java
@@ -11,12 +11,12 @@ package com.noworder.mb;
  */
  
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import com.noworder.mb.domain.Car;
+import java.util.Arrays;
  
 @ManagedBean(name = "carService")
 @ApplicationScoped
@@ -40,16 +40,16 @@ public class CarService {
         colors[9] = "Maroon";
          
         brands = new String[10];
-        brands[0] = "Produto A";
-        brands[1] = "Produto B";
-        brands[2] = "Produto C";
-        brands[3] = "Produto D";
-        brands[4] = "Produto E";
-        brands[5] = "Produto F";
-        brands[6] = "Produto G";
-        brands[7] = "Produto H";
-        brands[8] = "Produto I";
-        brands[9] = "Produto J";
+        brands[0] = "BMW";
+        brands[1] = "Mercedes";
+        brands[2] = "Volvo";
+        brands[3] = "Audi";
+        brands[4] = "Renault";
+        brands[5] = "Fiat";
+        brands[6] = "Volkswagen";
+        brands[7] = "Honda";
+        brands[8] = "Jaguar";
+        brands[9] = "Ford";
     }
      
     public List<Car> createCars(int size) {
